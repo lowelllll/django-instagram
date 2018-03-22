2e829e1f452a4ff427a20996350ff0a83523aa96
Lowellll <yejin2651@naver.com>
2018-03-22 20:37:59 +0900

    post date filter 수정
    date 잘못 표시되는 오류 수정

--- django_instagram/post/templatetags/custom_tags.py
+++ django_instagram/post/templatetags/custom_tags.py
@@ -9,14 +9,14 @@ def post_date(upload_date): # date 필터
     now_date = datetime.now(timezone.utc)
     day = now_date - upload_date
     if day.days!=0:
-        if day.days > 364:
+        if day.days > 364 | upload_date.year != now_date.year:
             return "{0}년 {1}월 {2}일".format(upload_date.year,upload_date.month,upload_date.day)
         elif day.days >= 31:
             return "{0}달 전".format(round(day.days/31))
         else:
             return "{0}일 전".format(day.days)
     else:
-        if upload_date.hour == now_date.hour:
-            return "{0}분 전".format(now_date.minute-upload_date.minute)
+        if day.seconds//60 < 60:
+            return "{0}분 전".format(day.seconds//60)
         else:
             return "{0}시간 전".format(day.seconds//3600)
