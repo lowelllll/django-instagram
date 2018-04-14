694e9a776d7b5129f34cc1aa58042ad899a324d1
Lowellll <yejin2651@naver.com>
2018-04-14 23:08:11 +0900

    auth.urls.py namespace 수정
    auth -> account

--- django_instagram/django_instagram/views.py
+++ django_instagram/django_instagram/views.py
@@ -8,4 +8,4 @@ from django.contrib.auth.forms import UserCreationForm # 장고 기본 폼 사�
 class Main(CreateView):
     template_name = 'registration/sign_up.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('auth:login')
+    success_url = reverse_lazy('account:login')
